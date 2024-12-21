275791dc7b19b34ac25c103ebd65d30c9822d8b5
Laith Malky <laith.malky@gmail.com>
2024-12-21 16:02:34 +0200

    commit day2

--- day2-final-project.py
+++ day2-final-project.py
@@ -6,10 +6,10 @@ tip = int(input("How much tip would you like to give? 10, 12, 15 ? "))
 people = int(input("How many people to split the bill? "))
 
 
-n = (bill / people) * (tip / 100 + 1)
+#n = (bill / people) * (tip / 100 + 1)
 tip_prc = tip / 100 
 total_bill = (tip_prc + 1) * bill
 pay_per_person = total_bill / people
 
 print(f"Each person should pay: ${round(pay_per_person, 2)}")
-print(f"Each person should pay: ${round(n, 2)}")
+#print(f"Each person should pay: ${round(n, 2)}")
